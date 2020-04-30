a7c9e8f6a33eb840d7013bea683710a856cd5fe4
Chris Piraino <cpiraino@hashicorp.com>
2020-04-30 16:27:47 -0500

    Fix api test

--- api/config_entry_test.go
+++ api/config_entry_test.go
@@ -651,6 +651,9 @@ func TestDecodeConfigEntry(t *testing.T) {
 			expect: &IngressGatewayConfigEntry{
 				Kind: "ingress-gateway",
 				Name: "ingress-web",
+				TLS: GatewayTLSConfig{
+					Enabled: true,
+				},
 				Listeners: []IngressListener{
 					IngressListener{
 						Port:     8080,
